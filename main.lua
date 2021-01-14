d3f73e93342cd15afdf447ffc6d1524c45d2d309
Xiangcheng Chen <nerdull@users.noreply.github.com>
2021-01-14 10:16:56 +0100

    reverted commit f503604

--- main.lua
+++ main.lua
@@ -6,17 +6,30 @@ License: GNU GPLv3
 --]]
 
 -- [[ define parameters
-a = 10 -- mm
-grid_unit = 0.1 -- mm
+n_ring = 6
+r_0 = 3 -- mm
+r_m = r_0 * 2 -- mm
+d = 3.4 -- mm
+s = d * .6 -- mm
+grid_unit = 1e-3-- mm
+
+local freq = 2.22 -- MHz
+local V_0 = 96 -- V
 
 if pcall(debug.getlocal, 4, 1) then -- acting as an imported module
-    return { a = a }
+    local M = {}
+    M.n_ring = n_ring
+    M.d = d
+    M.freq = freq
+    M.V_0 = V_0
+    return M
 end
 --]]
 
 -- [[ build potential arrays
 if not pcall(debug.getlocal, 4, 1) then -- acting as a main file
-    local fname = "buffer_gas"
-    simion.command(string.format("gem2pa %s.gem %s.pa0", fname, fname))
+    local fname = "ion_guide"
+    simion.command(string.format("gem2pa %s.gem %s.pa#", fname, fname))
+    simion.command(string.format("refine %s.pa#", fname))
 end
 --]]
