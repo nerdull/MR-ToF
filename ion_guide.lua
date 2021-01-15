91041a60c37ef507b9123614433bfc1b900fd13d
Xiangcheng Chen <nerdull@users.noreply.github.com>
2021-01-15 10:13:08 +0100

    added more options to x-position tracking

--- ion_guide.lua
+++ ion_guide.lua
@@ -14,14 +14,17 @@ adjustable _freq = M.freq -- MHz
 adjustable _V_0 = M.V_0 -- V
 
 local HS1 = simion.import "collision_hs1.lua"
-adjustable _pressure_pa = 0 -- disable buffer gas
+adjustable _pressure_pa = 0.5 -- set 0 to disable buffer gas, Pa
 adjustable _trace_level = 2 -- keep an eye on ion's kinetic energy
 
 local WAVE = simion.import "waveformlib.lua"
 
 local last_pos_print = 0 -- micro-s
-local pos_print_period = 10 -- print ion's x-pos every this number of micro-s, or when it is teleported
 local teleport_dist = 0 -- accumulated distance over which ion has been teleported so far, mm
+-- print ion's x-pos every this number of micro-s, or when it is teleported
+-- set 0 to print every ion_time_step
+-- set an negative value to disable print
+adjustable _pos_print_period = 100
 
 function segment.initialize_run()
     WAVE.install {
@@ -82,12 +85,11 @@ function segment.other_actions()
     end
     -- [[ regularly print x-position
     function pos_print()
+        if _pos_print_period < 0 then return end
         print("position = " .. ion_px_mm-teleport_dist .. " mm")
         last_pos_print = ion_time_of_flight
     end
-    if ion_time_of_flight - last_pos_print > pos_print_period then
-        pos_print()
-    end
+    if ion_time_of_flight - last_pos_print > _pos_print_period then pos_print() end
     --]]
     -- [[ periodic boundary condition
     local left_boundary = -d * (n_ring-2) -- mm
