b4ddb9f5388fe4d9015c43bae03d779f5d49947f
Xiangcheng Chen <nerdull@users.noreply.github.com>
2021-01-16 22:55:00 +0100

    exported potential arrays

--- main.lua
+++ main.lua
@@ -6,30 +6,35 @@ License: GNU GPLv3
 --]]
 
 -- [[ define parameters
-n_ring = 6
-r_0 = 3 -- mm
+n_ring = 3
+r_0 = 1 -- mm
 r_m = r_0 * 2 -- mm
-d = 3.4 -- mm
-s = d * .6 -- mm
+d = 1 -- mm
 grid_unit = 1e-3-- mm
-
-local freq = 2.13 -- MHz
-local V_0 = 72 -- V
-
-if pcall(debug.getlocal, 4, 1) then -- acting as an imported module
-    local M = {}
-    M.n_ring = n_ring
-    M.d = d
-    M.freq = freq
-    M.V_0 = V_0
-    return M
-end
 --]]
 
--- [[ build potential arrays
-if not pcall(debug.getlocal, 4, 1) then -- acting as a main file
-    local fname = "ion_guide"
+-- [[ calculate potential arrays and export them
+local fname = "electrode_thickness"
+local n_z = math.ceil(d/2 / grid_unit)
+local n_r = math.ceil(r_0 / grid_unit)
+
+for percent=45,70,1 do
+    s = percent / 100 * d -- mm
     simion.command(string.format("gem2pa %s.gem %s.pa#", fname, fname))
     simion.command(string.format("refine %s.pa#", fname))
+    simion.command(string.format("fastadj %s.pa0 1=1", fname))
+    local pa = simion.pas:open(string.format("%s.pa0", fname))
+    local f = io.open(string.format("%s_%d_percent.txt", fname, percent), 'w')
+
+    for j = 0,n_r do
+        for i=0,n_z do
+            local phi = pa:potential(i, j, 0)
+            f:write(string.format("%g ", phi))
+        end
+        f:write('\n')
+    end
+
+    f:close()
+    pa:close()
 end
 --]]
